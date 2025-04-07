513c9b6fdbaf9355d24abed93a5cbadd844fb9c6
maedachaeva <parkchaeyeon.b@gmail.com>
2025-04-07 13:02:03 +0900

    feat: [250327] 08_vue 기본 과제 제출
    - Composition API

---
+++ 02-vue-interface/08-vue-basic/calc-component-test/src/components/Calc.vue
@@ -0,0 +1,23 @@
+<!-- setup 메서드를 이용한 초기화 -->
+
+<template>
+    <div>
+        X : <input type="text" v-model.number="x" /><br />
+        Y : <input type="text" v-model.number="y" /><br />
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    name: 'Calc',
+    setup() {
+        const x = ref(10);
+        const y = ref(20);
+        // const x = 10;
+        // const y = 20; 가능은 한데 반응성이 없음
+        return { x, y };
+    },
+};
+</script>
---
+++ 02-vue-interface/08-vue-basic/calc-component-test/src/components/Calc6.vue
@@ -0,0 +1,27 @@
+<!-- watch2 핸들러 중복 호출 해결하는 법(state.x) p291-->
+
+<template>
+    <div>
+        x : <input type="text" v-model.number="state.x" /><br />
+        결과 : {{ state.result }}
+    </div>
+</template>
+
+<script>
+import { reactive, watch } from 'vue';
+
+export default {
+    name: 'Calc6',
+    setup() {
+        const state = reactive({ x: 0, result: 0 });
+        watch(
+            () => state.x,
+            (current, old) => {
+                console.log(`${old} -> ${current}`);
+                state.result = current * 2;
+            }
+        );
+        return { state };
+    },
+};
+</script>
